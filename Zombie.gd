41946707a3b413b4affb6277ada658eb07062b43
Levi Schneider <levi.schneider2010@gmail.com>
2019-07-16 22:37:40 -0400

    The initial commit

---
+++ Zombie.gd
@@ -0,0 +1,33 @@
+extends KinematicBody2D
+
+const MOVE_SPEED = 300
+
+onready var raycast = $RayCast2D
+
+var player = null
+
+func _ready():
+	add_to_group("zombies")
+	
+func _physics_process(delta):
+	if player == null:
+		return
+			
+	var vec_to_player = player.global_position - global_position
+	vec_to_player = vec_to_player.normalized()
+	global_rotation = atan2(vec_to_player.y, vec_to_player.x)
+	move_and_collide(vec_to_player * MOVE_SPEED * delta)
+	
+	var look_vec = get_global_mouse_position() - global_position
+	global_rotation = atan2(look_vec.y, look_vec.x)
+	
+	if raycast.is_colliding():
+		var coll = raycast.get_collider()
+		if coll.name == "Player":
+			coll.kill()
+			
+func kill():
+	queue_free()
+	
+func set_player(p):
+	player = p
